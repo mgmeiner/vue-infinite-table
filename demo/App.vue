0b191778f9085d316ff691d317f5a170f5da22ca
mgmeiner <gmeiner.m@gmail.com>
2017-01-31 20:21:11 +0100

    add first prototype

--- demo/App.vue
+++ demo/App.vue
@@ -1,10 +1,32 @@
 <template>
-    <h4>INFINITE_TABLE</h4>
+  <div>
+    <h1>Infinite Table DEMO</h1>
+
+    <infiniteTable :columns="columns" :data="data" @consumeData="onConsumeData" />
+  
+  </div>
 </template>
 
 <script>
+  import infiniteTable from '../lib/InfiniteTable';
+  import sampleData from './data.json';
+
   export default {
-    name: 'app'
+    name: 'app',
+    data () {
+      return {
+        data: [],
+        columns: ['id', 'name', 'country']
+      }
+    },
+    components: {
+      infiniteTable
+    },
+    methods: {
+      onConsumeData (dataOptions) {
+        this.data.push.apply(this.data, sampleData.companies.slice(dataOptions.offset, dataOptions.endIndex));
+      }
+    }
   };
 </script>
 
--- lib/InfiniteTable.vue
+++ lib/InfiniteTable.vue
@@ -1,12 +1,63 @@
 <template>
-    <h1>Infinite Table</h1>
+  <div class="vueInfiniteTable">
+    <table>
+      <tbody>
+        <tr v-for="row in data">
+          <td v-for="column in columns">{{row[column]}}</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: 'infiniteTable'
+  export default {
+    name: 'vueInfiniteTable',
+    props: {
+      data: {
+        type: Array,
+        required: true
+      },
+      columns: {
+        type: Array,
+        required: true
+      },
+      options: {
+        type: Object,
+        default: {
+          pageSize: 10
+        }
+      }
+    },
+    data () {
+      return {
+        dataOptions: {
+          offset: 0,
+          pageSize: this.options.pageSize,
+          endIndex: this.options.pageSize
+        }
+      }
+    },
+    created () {
+      this.consumeData();
+    },
+    methods: {
+      pageEnd () {
+        this.dataOptions.offset += this.options.pageSize;
+        this.dataOptions.endIndex += this.options.pageSize;
+        this.consumeData();
+      },
+      consumeData () {
+        this.$emit('consumeData', this.dataOptions);
+      }
     }
+  }
 </script>
 
-<style>
+<style lang="less">
+  .vueInfiniteTable {
+    table {
+      width: 100%;
+    }
+  }
 </style>
